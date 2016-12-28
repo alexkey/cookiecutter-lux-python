9620e106e3644136ccb6c07a584912fb8cd8c5e3
Alex Key <alexk3y@gmail.com>
2016-12-28 16:22:58 +0300

    Use new-style configuration for a paper size in LaTeX

--- {{ cookiecutter.repo_name }}/doc/conf.py
+++ {{ cookiecutter.repo_name }}/doc/conf.py
@@ -246,7 +246,7 @@ htmlhelp_basename = '{{ cookiecutter.repo_name | replace('_', '') }}doc'
 latex_elements = {
      # The paper size ('letterpaper' or 'a4paper').
      #
-     # 'papersize': 'letterpaper',
+     'papersize': 'a4paper',
 
      # The font size ('10pt', '11pt' or '12pt').
      #
