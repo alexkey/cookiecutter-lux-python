1353c61ced6fbe2f0c6ad2ac7cc9a36b4931547f
Alex Key <alexk3y@gmail.com>
2017-03-15 14:54:02 +0300

    Fix Pytest invocation style

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -38,14 +38,14 @@ class PyTest(TestCommand):
     user_options = [('pytest-args=', 'a', "Arguments to pass to pytest")]
 
     def initialize_options(self):
-        TestCommand.initialize_options(self)
+        super().initialize_options()
         self.pytest_args = []
 
     def run_tests(self):
         # Import here, cause outside the eggs aren't loaded.
         import pytest
 
-        errno = pytest.main(self.pytest_args)
+        errno = pytest.main(self.pytest_args.split())
         sys.exit(errno)
 
 
