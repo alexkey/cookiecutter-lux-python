5e2e09f91dca428dd2995b37d8537f511c6a6a28
Alex Key <alexk3y@gmail.com>
2017-03-15 14:39:25 +0300

    Remove superfluous stuff
     - Additional shell sourcing.
    
     - NodeJS installation at bootstrap.
    
     - Redis installation at bootstrap.

--- {{ cookiecutter.repo_name }}/script/bootstrap.sh
+++ {{ cookiecutter.repo_name }}/script/bootstrap.sh
@@ -81,44 +81,6 @@ EOF
         > "${HOME}/.pip/pip.conf"
 
     pip -q install -U ${PIP_DEPS[*]}
-
-    {%- if cookiecutter.enable_nodejs == 'true' %}
-
-    # Install Node.js v{{ cookiecutter.nodejs_version }}.
-    wget -qO- https://deb.nodesource.com/setup_{{ cookiecutter.nodejs_version }}.x | sudo -E bash -
-    sudo apt-get install -qy nodejs
-    {%- endif %}
-
-    {%- if cookiecutter.enable_redis == 'true' %}
-
-    # Install Redis {{ cookiecutter.redis_version }}.
-    local redis_version="{{ cookiecutter.redis_version }}"
-
-    apt-get install -yq tcl8.5
-
-    wget -q http://download.redis.io/releases/redis-${redis_version}.tar.gz
-    tar xzf redis-${redis_version}.tar.gz
-
-    (cd redis-${redis_version} && make test install)
-
-    (cd redis-${redis_version}/utils && \
-        sed -r "s/^\s*(read\s+-p)/# \1/g" -i install_server.sh && \
-        \
-        REDIS_PORT={{ cookiecutter.redis_port }} \
-        REDIS_CONFIG_FILE={{ cookiecutter.redis_config_file }} \
-        REDIS_LOG_FILE={{ cookiecutter.redis_log_file }} \
-        REDIS_DATA_DIR={{ cookiecutter.redis_data_dir }} \
-        REDIS_EXECUTABLE={{ cookiecutter.redis_executable }} \
-            ./install_server.sh)
-
-    update-rc.d redis_{{ cookiecutter.redis_port }} defaults
-    {%- endif %}
-
-    {%- if cookiecutter.vagrant_shell_source %}
-
-    # Additional shell source for a custom setup.
-    source {{ cookiecutter.vagrant_shared_guest }}/{{ cookiecutter.vagrant_shell_source }}
-    {%- endif %}
 }
 
 
